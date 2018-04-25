0652904f20e639a60284c6654cf98ab84debd001
Jordan <takihirocore@gmail.com>
2018-04-24 20:12:38 -0700

    [Config] Protocal
    Switching to HTTP for the time being, will implement with HTTPS later.

--- config/config.js
+++ config/config.js
@@ -4,7 +4,7 @@ module.exports = function(env) {
     let config = {};
     if (env === "production") {
         config.path = "/home/bitnami/stack/resources/files";
-        config.port = 443;
+        config.port = 80; // TODO: Change to 443 at some point to support https
     } else {
         config.path = helpers.root("files");
         config.port = 8082;
