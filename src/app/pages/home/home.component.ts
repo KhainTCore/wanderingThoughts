c3c31d374aef505f872281ff8ccffcc05ac684dd
KhainTCore <takihirocore@gmail.com>
2018-04-27 01:02:18 -0700

    [Bug Fix] Home Page Default
    - Default Text appears when HTTP req fails
    - Validate if file returned before operating on it

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -21,10 +21,12 @@ export class HomeComponent implements OnInit {
 
     ngOnInit() {
         this.fileFetcherService.fetchStaticPage("h").subscribe((file: any) => {
-            for (let line of file.file.body) {
-                if (line.match(/^<h[2-9]>/))
-                    this.content.push("mat-divider");
-                this.content.push(line);
+            if (file.file.body) {
+                for (let line of file.file.body) {
+                    if (line.match(/^<h[2-9]>/))
+                        this.content.push("mat-divider");
+                    this.content.push(line);
+                }
             }
         });
     }
